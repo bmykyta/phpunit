67f2b4ec4c0610afd7bfa1ffbd4e2b689e9036c7
Mykyta <>
2022-07-26 22:51:18 +0300

    Add DinosaurLengthDeterminator test case

---
+++ tests/Service/DinosaurLengthDeterminatorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Dinosaur;
+use App\Service\DinosaurLengthDeterminator;
+use PHPUnit\Framework\TestCase;
+
+class DinosaurLengthDeterminatorTest extends TestCase
+{
+    /**
+     * @dataProvider getSpecLengthTests
+     */
+    public function testItReturnsCorrectLengthRange($spec, $minExpectedSize, $maxExpectedSize): void
+    {
+        $determinator = new DinosaurLengthDeterminator();
+        $actualSize   = $determinator->getLengthFromSpecification($spec);
+        $this->assertGreaterThanOrEqual($minExpectedSize, $actualSize);
+        $this->assertLessThanOrEqual($maxExpectedSize, $actualSize);
+    }
+
+    public function getSpecLengthTests()
+    {
+        return [
+            // specification, min length and max length
+            ['large carnivorous dinosaur', Dinosaur::LARGE, Dinosaur::HUGE - 1],
+            'default response' => ['give me all the cookies!!!', 0, Dinosaur::LARGE - 1],
+            ['large herbivore', Dinosaur::LARGE, Dinosaur::HUGE - 1],
+            ['huge dinosaur', Dinosaur::HUGE, 100],
+            ['huge dino', Dinosaur::HUGE, 100],
+            ['huge', Dinosaur::HUGE, 100],
+            ['OMG', Dinosaur::HUGE, 100],
+            ['😱', Dinosaur::HUGE, 100],
+        ];
+    }
+}
